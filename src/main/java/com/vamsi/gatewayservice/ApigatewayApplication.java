71b51eaa35c7a04f5dde5039edcdf648b3355422
vamsi <vamsyr13@gmail.com>
2024-06-03 12:09:38 -0400

    Test commit

--- src/main/java/com/vamsi/gatewayservice/ApigatewayApplication.java
+++ src/main/java/com/vamsi/gatewayservice/ApigatewayApplication.java
@@ -8,7 +8,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class ApigatewayApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+		System.out.println("Hello!");
 		SpringApplication.run(ApigatewayApplication.class, args);
 	}
 
